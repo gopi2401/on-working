4c803719138dba9d9c04e8e436e459957ba0bd3f
gopi2401 <gobinath@streamflo.io>
2023-03-01 19:07:46 +0530

    test

--- testcase/test/utils/arr.tes.ts
+++ testcase/test/utils/arr.tes.ts
@@ -1,8 +1,9 @@
 import { describe, expect, test } from '@jest/globals';
 import { split2Arr } from '../../src/utils/arr';
-const input = "skewX(25deg)"
-const output = ["skewX(25deg)"]
-
-test('', () => {
-    expect(split2Arr(input)).toBe(output);
+const input = "skewX(25deg) rotate3d(180deg,0,1) scale3d(2,2,1) translate3d(10px,10px,0px)"
+const output = ["skewX(25deg)", "rotate3d(180deg,0,1)", "scale3d(2,2,1)", "translate3d(10px,10px,0px)"]
+describe('key word ) split to array ', () => {
+    test('', () => {
+        expect(split2Arr(input)).toEqual(output);
+    });
 });
